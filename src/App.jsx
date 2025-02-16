e09c3092290f31d404d0e975b72f95aa1eeb88d6
Kwandile Mgenge <kwandilen.mgenge@gmail.com>
2025-02-16 12:00:12 +0200

    BrowserRouter Initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={}/> */}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
