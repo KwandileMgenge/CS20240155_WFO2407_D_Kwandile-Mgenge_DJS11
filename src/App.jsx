158047d73b089b6145dcb698372add9d8cd352fc
Kwandile Mgenge <kwandilen.mgenge@gmail.com>
2025-02-16 12:12:22 +0200

    Icon and Logo Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={}/> */}
+        <Route path="/" element={<HomePage/>}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div className='logo'>
+      <h1>Pod<span>lib</span></h1>
+    </div>
+  )
+}
+
+export default Logo
