3328c7eff85e0935a692a1e3103884168e03c351
Kwandile Mgenge <kwandilen.mgenge@gmail.com>
2025-02-16 12:38:12 +0200

    HomePage Implementation and Design

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import Logo from '../components/Logo'
+import Headphones from '../assets/headphones.png'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
   return (
-    <div>
+    <div className='homepage'>
       <Logo/>
+
+      <div className='homepage-content'>
+        <div>
+          <img src={Headphones} alt="headphones icon" />
+          <h2>Listen</h2>
+        </div>
+
+        <div>
+          <h3>
+            Anytime. Anywhere.
+          </h3>
+        </div>
+
+        <Link to={'/previews'}>
+          <button>
+            Browser
+          </button>
+        </Link>
+      </div>
     </div>
   )
 }
