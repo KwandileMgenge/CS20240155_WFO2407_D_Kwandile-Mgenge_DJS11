02388a43d225966d1ebf9606fcc04c74a36e8fc1
Kwandile Mgenge <kwandilen.mgenge@gmail.com>
2025-02-16 14:14:06 +0200

    Previews Tile Design

--- src/components/Previews.jsx
+++ src/components/Previews.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 function Previews() {
   const { status, error, data: previews } = useQuery({
-    queryKey: ['previews'],
+    queryKey: ['previews-data'],
     queryFn: getPodcastPreviews,
   })
 
@@ -14,7 +14,20 @@ function Previews() {
   const sortedPreviews = [...previews].sort((a, b) => a.title.localeCompare(b.title))
 
   return (
-    <div>Previews</div>
+    <div className='previews-page'>
+      <h2>Explore Podcasts</h2>
+      <div className='previews-container'>
+        {sortedPreviews.map((podcast) => (
+          <div className='podcast-tile' key={podcast.id}>
+            <div className='podcast-img'>
+              <img src={podcast.image} alt="podcast image" />
+            </div>
+            <h3>{podcast.title}</h3>
+            <p>{podcast.updated.slice(0,10)}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
